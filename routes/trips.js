55a4b333ff77565485e4c3f3f428f36d67ed9b67
Simon Apartis <simon.apartis@cnrs.fr>
2024-11-06 19:54:13 +0100

    moment grâce à l'aide du chat

--- routes/trips.js
+++ routes/trips.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 require('../models/connection');
 const Trip = require('../models/trips');
+const moment = require('moment');
 
 router.post('/', (req, res) => {
+  // Date de début de la journée
+  const startOfDay = moment(req.body.date).startOf('day').toDate();
+  // Date de fin de la journée
+  const endOfDay = moment(req.body.date).endOf('day').toDate();
     Trip.find({
       departure : req.body.departure,
       arrival : req.body.arrival,
-      date : req.body.date
+      date: { $gte: startOfDay, $lte: endOfDay },
     })
     .then(data => {
         res.json({ allTrains: data });

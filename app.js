9f8bea9157bce404a081b22c7454fa5c8df131d9
Simon Apartis <simon.apartis@cnrs.fr>
2024-11-06 19:38:32 +0100

    autorisation de tous les accès distants sur cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var tripsRouter = require('./routes/trips');
 
 var app = express();
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
